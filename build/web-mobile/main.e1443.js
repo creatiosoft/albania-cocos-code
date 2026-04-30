e3c19f7131cb6b6ca089637a46bac5c9e1cb35b7
creatiosoft-Preet <preet@creatiosoft.com>
2026-04-30 14:54:20 +0530

    md5 enable

--- build/web-mobile/main.e1443.js
+++ build/web-mobile/main.e1443.js
@@ -163,7 +163,7 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.js');
+        require('src/settings.56891.js');
         require('src/cocos2d-runtime.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
@@ -171,7 +171,7 @@ if (window.jsb) {
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.js');
+        require('src/settings.56891.js');
         require('src/cocos2d-jsb.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
--- build/web-mobile/src/settings.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/resources/Portrait.fire",orientation:"portrait",jsList:["assets/plugins/socket.io.min.js"]};
